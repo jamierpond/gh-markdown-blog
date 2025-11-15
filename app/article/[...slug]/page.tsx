54a90274014decc2135912a936339f723cbf9c8d
Jamie Pond <jamiepond259@gmail.com>
2025-11-14 18:09:05 -0800

    wip

--- app/article/[...slug]/page.tsx
+++ app/article/[...slug]/page.tsx
@@ -1,9 +1,16 @@
-import { PageProps } from "@/.next/types/app/layout";
 import { MarkdownView } from "@/app/Components/MarkdownView";
 import { getFileContent, getLastUpdated, getUsername, extractTitle } from "@/app/shared";
 import { notFound } from "next/navigation";
 
 export const dynamic = 'force-dynamic';
+
+interface Params {
+  slug: string[];
+}
+
+interface PageProps {
+  params: Promise<Params>;
+}
 
 export default async function Page({ params }: PageProps) {
   const username = await getUsername();
