e89adef7249e30564a39b73d5667522183b1db60
Jamie Pond <jamiepond259@gmail.com>
2025-12-12 19:07:43 -0800

    wip

--- apps/blog.pond.audio/app/[...slug]/page.tsx
+++ apps/blog.pond.audio/app/[...slug]/page.tsx
@@ -1,15 +1,14 @@
-import { renderMadeaBlog, renderPage } from 'madea-blog-core';
+import { renderMadeaBlogPage } from 'madea-blog-core';
 import { createBlogConfig } from '@/app/lib/madea-config';
 
 export const dynamic = 'force-dynamic';
+
+const CONFIG = createBlogConfig();
 
 interface PageProps {
   params: Promise<{ slug: string[] }>;
 }
 
 export default async function Page({ params }: PageProps) {
-  const { slug } = await params;
-  const config = createBlogConfig();
-  const result = await renderMadeaBlog(config, slug.join('/'), { hasUsername: true });
-  return renderPage(result);
+  return renderMadeaBlogPage(CONFIG, params);
 }
