b6334d371d2366467b1d44ab7aca6f64348cbe3e
Jamie Pond <jamiepond259@gmail.com>
2025-12-12 19:00:26 -0800

    wip

--- apps/blog.pond.audio/app/page.tsx
+++ apps/blog.pond.audio/app/page.tsx
@@ -1,22 +1,11 @@
-import { renderMadeaBlog } from 'madea-blog-core';
+import { renderMadeaBlog, renderPage } from 'madea-blog-core';
 import { createBlogConfig } from './lib/madea-config';
+import { notFound } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   const config = createBlogConfig();
   const result = await renderMadeaBlog(config, '/', { hasUsername: true });
-
-  switch (result.type) {
-    case 'article':
-      return <result.View {...result.props} />;
-    case 'file-browser':
-      return <result.View {...result.props} />;
-    case 'no-repo-found':
-      return <result.View {...result.props} />;
-    case 'landing':
-      return <result.View {...result.props} />;
-    case '404':
-      return <div>Page not found</div>;
-  }
+  return renderPage(result, notFound);
 }
