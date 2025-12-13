203a665113fdded8bda2111123a1ec8ff1f0b86a
jamie <jamiepond@jamies-MacBook-Pro.local>
2025-12-13 03:15:57 -0800

    wip

--- apps/golf-blog/app/page.tsx
+++ apps/golf-blog/app/page.tsx
@@ -1,10 +1,7 @@
 import { renderMadeaBlogPage } from 'madea-blog-core';
 import { createBlogConfig } from './lib/madea-config';
-
 export const dynamic = 'force-dynamic';
-
 const CONFIG = createBlogConfig();
-
 export default async function Page() {
   return renderMadeaBlogPage(CONFIG);
 }
